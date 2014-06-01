3f95bae021a776257687f7da18cb1d7d22b2dc8c
Zhanhui Li <lizhanhui@gmail.com>
2014-06-01 13:42:08 +0800

    add test cases for number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Main {
     private int lfie;
+
+    private String goodName;
+
+    private String http;
+
+    private String realTimeHttp;
+
+    private int htmlCssJavascript;
+
+    private boolean lifeSucks;
+
+    private static final int REALLY_LONG_CONSTANT_DEFINITION = 1;
+
+    private static final long REALLY_LONG_CONSTANT_DEFINITION_LONG = 1L;
+
+    private static float  REALLY_LONG_CONSTANT_DEFINITION_FLOAT = 0X1F;
+
+    private static double REALLY_LONG_CONSTANT_DEFINITION_DOUBLE = 1.0d;
+
+    //constant has bad name
+    private static final int REALLY_LNOG_CONSTANT_DEFINITION = 1;
+
+    private DateFormat dateFormat = new SimpleDateFormat();
 
     public static void main(String[] args) {
         System.out.println("OK");
